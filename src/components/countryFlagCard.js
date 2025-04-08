d5a2e04e8c0f95878df8a8578a2e5556dac2a0dd
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-04-08 13:17:51 +0530

    made changes

--- src/components/countryFlagCard.js
+++ src/components/countryFlagCard.js
@@ -1,18 +1,7 @@
+import "./countryFlagCard.css"
 export function CountryFlagCard({name, flag }) {
     return (
-      <div style ={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "0.5px solid black",
-        gap: "4px",
-        height: "190px",
-        width: "190px",
-        borderRadius: "5px",
-        objectFit: "cover",
-        overflow: "hidden",
-      }}>
+      <div className="countryCard">
         <div style ={{
           display:"flex",
           justifyContent: "center",
